f02ab4bd68cf581119b4ebf27fc4ee2b34200e86
repl.it user <replituser@example.com>
2020-08-29 05:07:42 +0000

    project finished by MZ

--- server.js
+++ server.js
@@ -3,21 +3,19 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+const dns = require('dns');
 var cors = require('cors');
 
 var app = express();
 
-// Basic Configuration 
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -25,11 +23,59 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+const Schema = mongoose.Schema
+
+const shortenerSchema = new Schema({
+  original_url: String,
+  short_url: String
+})
+
+const Shortener = mongoose.model("Shortener", shortenerSchema)
+
+const getCount = async () => {
+  const count = await Shortener.find({})
+  return count
+}
+
+const checkUrl = async (url) => {
+  let isValid = true;
+    dns.lookup(`${url.slice(url.indexOf('.')+1)}`, (err, addresses,family) => {
+      if (err) isValid = false
+    })
+  return isValid
+}
+
+app.post("/api/shorturl/new", (req, res) => {
+  getCount().then(result => {
+    const url = req.body.url
+    const isUrl = url.match(/^http[s]?[:][/]{2}www[.][a-zA-Z0-9]+[.][a-zA-Z]{2,4}$/g)
+    if(isUrl) {
+      checkUrl(url).then(exists => {
+        res.json({original_url: url, short_url: result.length+1})
+        const objToSave = new Shortener({
+          original_url: url,
+          short_url: result.length+1
+        })
+        objToSave.save((err,data) => {
+          if (err) return console.log(err)
+          console.log(data)
+        })  
+      }).catch(doesnt => {
+        console.log("Url does not exist: "+doesnt.data)
+      })
+    } else {
+      res.json({error: "invalid URL"})
+    }}).catch(err => {
+      console.log(err)
+    })      
+})
+app.get(`/api/shorturl/:short`, (req, res) => {
+  Shortener.findOne({short_url: req.params.short}, (err, foundUrl) => {
+    if(err) return console.log(err)
+    console.log(foundUrl)
+    res.redirect(foundUrl.original_url)
+  })
+})
 
 
 app.listen(port, function () {
